4c225484e45bf6fed5e0c19d95d951b6157b3966
dincerdegre <dincerdegre@gmail.com>
2020-10-11 15:34:39 +0300

    Create age-from-birthyear.php

---
+++ age-from-birthyear.php
@@ -0,0 +1,19 @@
+<?php
+// Age From Birthday (Birthyear)
+// Doğum gününden (Yılından) Yaş Hesaplama
+class DegreClass {
+public $birthyear;
+
+public function agefromBirthyear(){
+if (isset($this->birthyear)):
+$yearnow = date("Y");
+$age = $yearnow - $this->birthyear;
+return $age;
+endif;
+}
+
+}
+$degre = new DegreClass();
+$degre->birthyear = 1984;
+$degre->agefromBirthyear();
+?>
